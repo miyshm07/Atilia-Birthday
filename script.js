2c0f8efba0c16f1e274678929f9d55fdf0cbdde9
miyshm07 <mdhakimarmor@gmail.com>
2026-05-29 15:38:51 +0800

    first website upload

---
+++ script.js
@@ -0,0 +1,57 @@
+setTimeout(() => {
+    document.querySelector(".loading-screen").style.display = "none";
+}, 2500);
+
+const music = document.getElementById("bg-music");
+const musicBtn = document.getElementById("music-btn");
+
+musicBtn.addEventListener("click", () => {
+
+    if(music.paused){
+        music.play();
+        musicBtn.innerText = "⏸ Pause Music";
+    }else{
+        music.pause();
+        musicBtn.innerText = "🎵 Play Music";
+    }
+
+});
+
+document.querySelector(".secret-star")
+.addEventListener("click", () => {
+
+    alert("you found the secret message: you're genuinely important to me 🤍");
+
+});
+
+function showMessage(type){
+
+    const output = document.getElementById("letter-output");
+
+    if(type === "sad"){
+        output.innerText =
+        "everything will pass eventually, including bad days 🤍";
+    }
+
+    if(type === "tired"){
+        output.innerText =
+        "go drink water and sleep before becoming a zombie";
+    }
+
+    if(type === "miss"){
+        output.innerText =
+        "skill issue honestly";
+    }
+
+}window.onbeforeunload = function () {
+    return "you haven't finished being appreciated yet";
+};
+
+document.getElementById("cake-btn")
+.addEventListener("click", () => {
+
+    document.getElementById("cake-message")
+    .innerText =
+    "WISH ACCEPTED ✨";
+
+});
